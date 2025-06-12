e7baaf3934598d0317d78b9d8443849db3b1da5b
Jared-Ha <jared.harrison7@gmail.com>
2025-06-12 14:30:55 +0100

    add skills section and tech icon data file (actual svgs to dl later)

---
+++ src/data/techIcons.js
@@ -0,0 +1,21 @@
+const techIcons = {
+  React: "/icons/react.svg",
+  "React Native": "/icons/react-native.svg",
+  "Node.js": "/icons/nodejs.svg",
+  TypeScript: "/icons/typescript.svg",
+  JavaScript: "/icons/javascript.svg",
+  PostgreSQL: "/icons/postgresql.svg",
+  PHP: "/icons/php.svg",
+  HTML: "/icons/html5.svg",
+  CSS: "/icons/css3.svg",
+  Jest: "/icons/jest.svg",
+  Supertest: "/icons/supertest.svg",
+  GitHub: "/icons/github.svg",
+  WordPress: "/icons/wordpress.svg",
+  Supabase: "/icons/supabase.svg",
+  "REST API": "/icons/api.svg",
+  "Google Maps": "/icons/maps.svg",
+  Agile: "/icons/agile.svg",
+};
+
+export default techIcons;
