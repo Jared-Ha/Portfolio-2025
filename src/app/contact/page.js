438ccb401c53922a869c8d5c2d48b9b8568abad1
Jared-Ha <jared.harrison7@gmail.com>
2025-06-12 14:33:19 +0100

    add contact route

---
+++ src/app/contact/page.js
@@ -0,0 +1,16 @@
+export default function Contact() {
+  return (
+    <section>
+      <h2 className="text-3xl font-bold mb-4">Contact</h2>
+      <p>
+        You can reach me at{" "}
+        <a
+          className="text-blue-600 underline"
+          href="mailto:jared.harrison7@gmail.com"
+        >
+          jared.harrison7@gmail.com
+        </a>
+      </p>
+    </section>
+  );
+}
