0c7381b15aa9a8706abcc5d395f857f762c59fdf
Jared-Ha <jared.harrison7@gmail.com>
2025-10-28 10:59:43 +0000

    add project preview to project nav (and clean up)

--- src/app/projects/page.js
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-  return (
-    <section>
-      <h2 className="text-3xl font-bold mb-4">Projects</h2>
-      <p>Here I'll showcase my key portfolio projects.</p>
-    </section>
-  );
-}
